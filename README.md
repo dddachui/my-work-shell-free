cb9e11f3b86f58ef32c689c6ef1fded729679d8b
wsy <1395168068@qq.com>
2022-06-01 22:43:54 +0800

    修改:system_info，兼容centos7和kylinsec

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # my-work-shell-free
+- system_info.sh 用于检测系统的硬件信息和软件信息
+  已测试过系统如下
+    - centos7
+    - KylinSec
--- system_info.sh
+++ system_info.sh
@@ -3,17 +3,29 @@
 #********************************************************************
 #Author: dddachui
 #Date: 2022-06-01
-#FileName： systeminfo.sh
+#FileName： system_info.sh
 #Description： Show system information
+#Version: 0.2
 #********************************************************************
+check_centos(){
+if [ -f "/etc/centos-release" ]; then
+    OS=$(cat /etc/centos-release)
+fi
+}
+check_kylinSec(){
+if [ -f "/etc/.kyinfo" ]; then
+    OS=$(cat /etc/.kyinfo  | grep dist_id | awk '{print $3}')
+fi
+}
 RED="\E[1;31m"
 GREEN="echo -e \E[1;32m"
 END="\E[0m"
+OS=null
+check_centos
+check_kylinSec
 $GREEN----------------------主机信息采集--------------------$END
 echo -e "主机名: $RED$(hostname)$END"
-#echo -e "IPADDR: $RED` ifconfig eth0|grep -Eo '([0-9]{1,3}\.){3}[0-9]
-#{1,3}' |head -n1`$END"
-echo -e "系统版本: $RED$(cat /etc/.kyinfo  | grep dist_id | awk '{print $3}')$END"
+echo -e "系统版本: $RED${OS}$END"
 echo -e "内核: $RED$(uname -r)$END"
 echo -e "CPU型号: $RED$(cat /proc/cpuinfo | grep 'model name' | uniq | cut -d: -f2)$END"
 echo -e "物理CPU数量: $RED$(cat /proc/cpuinfo| grep "physical id"| sort| uniq| wc -l)$END"
@@ -24,5 +36,4 @@ echo -e "实际可用内存: $RED$(free -h|grep Mem|tr -s ' ' : |cut -d : -f2)$E
 echo -e "物理内存容量： $RED$(dmidecode | grep -A16 "Memory Device$"|grep Size|grep -v No | awk '{sum+=$2}END{print sum}')M$END"
 echo -e "内存条数量:  $RED$(dmidecode | grep -A16 "Memory Device$"|grep Size|grep -v No| wc -l)条$END"
 echo -e "单根内存容量: \n $RED$(dmidecode | grep -A16 "Memory Device$"|grep Size|grep -v No)$END"
-#echo -e "DISK: $RED`lsblk |grep '^sd' |tr -s ' ' |cut -d " " -f4`$END"
 $GREEN---------------------------------------------------------$END
